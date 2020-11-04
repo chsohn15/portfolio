dd9d97af52a785fb98c991106d5e13de4b049661
chsohn15 <chsohn15@gmail.com>
2020-11-03 22:07:46 -0500

    add navbar and bootstrap stylesheet

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 const Header = () => {
     return (
-        <div>
-            <ul>
-                <li>My Story</li>
-                <li>Projects</li>
-                <li>Work</li>
-                <li>Contact Me</li>
-            </ul>
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+                <img
+                alt=""
+                src="https://www.freepnglogos.com/uploads/c-letter-logo-png-0.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+            />{' '}
+      Christina Sohn
+      <Nav className="mr-auto">
+      <Nav.Link href="#my-story">My Story</Nav.Link>
+      <Nav.Link href="#projects">Projects</Nav.Link>
+      <Nav.Link href="#work">Work</Nav.Link>
+      <Nav.Link href="#contact-me">Contact Me</Nav.Link>
+    </Nav>
+    </Navbar.Brand>
+  </Navbar>
     )
 }
 
