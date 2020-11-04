86d195003bc14c54d698cb4ed1fc480bb483ba7f
chsohn15 <chsohn15@gmail.com>
2020-11-03 20:01:19 -0500

    add jumbotron

--- portfolio/src/components/Intro.js
+++ portfolio/src/components/Intro.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const Intro = () => {
     return (
-        <h2>Hello, My Name is Christina, and I am a Full Stack Developer</h2>
+        <Jumbotron id="jumbotron" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?ixlib=rb-1.2.1&w=1000&q=80')"}}>
+            <h2>Hello, My Name is Christina, and I am a Full Stack Developer</h2>
+        </Jumbotron>
     )
 }
 
