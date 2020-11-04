e3da5047809b7a4bcb59e4203ded0abc934028c4
chsohn15 <chsohn15@gmail.com>
2020-11-03 23:07:14 -0500

    add card

--- portfolio/src/components/myStory.js
+++ portfolio/src/components/myStory.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const MyStory = () => {
     return (
         <div>
+            <Row>
+        <Col>
+          <Card>
+            <Card.Header>My Story</Card.Header>
+            <Row >
+              <Col md="8">
+                <Card.Body>
+                  <Card.Text>
+                    Some quick example text to build on the card title and
+                    make up the bulk of the card's content.
+                  </Card.Text>
+ 
+                </Card.Body>
+              </Col>
+              <Col md="4">
+                <Card.Img
+                  top
+                  width="100%"
+                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                  alt="Card image cap"
+                />
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
             <h2>My Story</h2>
             <p>Full stack web developer with experience in Ruby and Javascript frameworks. Through my experience as a high school English teacher for five years, I crafted creative lessons and projects to appeal to a diversity of students and meet their learning goals. My detailed attention to the voice of the consumer, my love of language construction, and my interest in an intuitive and transformative user experience translate seamlessly into the coding world.   </p>
             <p>Here is a list of some of the technologies I have been using recently:</p>
