11df60f7caf4344e064d768c99b46876a6939487
chsohn15 <chsohn15@gmail.com>
2020-11-04 22:39:36 -0500

    added card to work experience

--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -5,16 +5,28 @@ import Row from 'react-bootstrap/Row'
 
 const Work = () => {
     return (
-        <div>
-            <h2>Work Experience</h2>
-            <p>English Teacher, South County High School, Lorton, VA					       	08/2016 - 06/2020</p>
+        <Row style={{marginLeft: '30px', marginTop: '50px'}}>
+        <Col>
+          <Card>
+            <Card.Header>Work Experience</Card.Header>
+            <Row >
+
+                <Card.Body>
+                  <Card.Text>
+                  <p>English Teacher, South County High School, Lorton, VA					       	08/2016 - 06/2020</p>
             <ul>
                 <li>Successfully engaged students in grades 10-12 through varied methods including small group work, effective moderating of class discussion, and authentic assessments</li>
                 <li>Collaborated with grade-level teams to create cohesive literary units centered around engaging topics</li>
                 <li>Incorporated technology such as Google Suite and Canva into lesson plans</li>
                 <li>Set measurable and attainable goals for student progress in writing throughout the year</li>
             </ul>
-        </div>
+                </Card.Text>
+                </Card.Body>
+            
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     )
 }
 
