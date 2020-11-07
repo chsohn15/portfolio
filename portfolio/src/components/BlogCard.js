a1aafa20eecae434b2e27a332ff15ec34ccb2d9e
chsohn15 <chsohn15@gmail.com>
2020-11-06 23:08:57 -0500

    adding text to blogs

--- portfolio/src/components/BlogCard.js
+++ portfolio/src/components/BlogCard.js
@@ -5,16 +5,20 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 
-const BlogCard = () => {
+const BlogCard = (props) => {
+
+console.log(props.blog)
+
+const { title, thumbnail, content } = props.blog
+const preview = content.slice(0, 150)
 
 return(
     <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Img variant="top" src={thumbnail} />
         <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>{title}</Card.Title>
             <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
+            {preview + "..."}
             </Card.Text>
         </Card.Body>
   </Card>
